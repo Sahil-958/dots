280f2263dd089b9589f5590beb91559296852a12
Sahil <118348625+Sahil-958@users.noreply.github.com>
2023-11-26 13:54:07 +0530

    added options to choose icons

--- scripts/genQTGTK.sh
+++ scripts/genQTGTK.sh
@@ -1,22 +1,26 @@
 oomox-cli ~/.cache/wal/colors-oomox -o testing_cli
-generate="generate icons theme"
-doNotGenerate="do no generate icons theme"
+numix="numix"
+papirus="papirus"
+suru="suruplus"
+suru_aspromauros="suruplus_aspromauros"
+none="Do not Generate Icons"
 
-choice=$(printf "$generate\n$doNotGenerate" | rofi -dmenu) 
+choice=$(printf "$numix\n$suru\n$suru_aspromauros\n$papirus\n$none" | rofi -dmenu) 
 
 
-if [ "$choice" == "$generate" ] ; then
+if [ "$choice" != "$none" ] ; then
 #	showNotification $line_count &
-run_script="/opt/oomox/plugins/icons_numix/change_color.sh" 
+run_script="/opt/oomox/plugins/icons_$choice/change_color.sh" 
 
 $run_script -o testing_cli -d ~/.icons/testing_cli ~/.cache/wal/colors-oomox > >(while read -r line; do
         chk=$((chk +$(echo $line|wc -l)))
-        progress=$(( (chk - 1) * 100 / 6  ))
-	dunstify -u normal -t 10000 -r 1337 -h int:value:$progress "Generating Icons Colorscheme..." 
+        progress=$(( (chk - 1) * 100 / 5  ))
+	dunstify -t 100000 -h int:value:$progress "Generating Icons Colorscheme..." 
 done)
 
-dunstify -u normal -r 1337  "Icon ColorScheme Generation Completed" 
-#dunstify -u normal -C 1337 
+dunstify   "Icon ColorScheme Generation Completed" 
+sleep 2
+dunstctl close
 
 fi
 
