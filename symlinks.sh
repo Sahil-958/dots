6ab2d305ec262848c53ab3028283b038d025dc93
Sahil <118348625+Sahil-958@users.noreply.github.com>
2023-12-28 19:08:45 +0530

    script to set up symlinks

---
+++ symlinks.sh
@@ -0,0 +1,19 @@
+# FOR PACKAGE LIST UPDATION
+ln -s ~/dots/package_list_contorl/packgae_update.sh /usr/bin
+ln -s ~/dots/package_list_contorl/update_package_list.hook /etc/pacman.d/hooks/
+ln -s ~/dots/package_list_contorl/pkglist_hooks_generated.txt /etc/pacman.d/hooks/
+ln -s ~/dots/package_list_contorl/pkglist_forgien_hooks_generated.txt /etc/pacman.d/hooks/
+
+# FOR HYPRLAND AND OTHER GUI UTILS
+ln -s ~/dots/hypr ~/.config/
+ln -s ~/dots/waybar ~/.config/
+ln -s ~/dots/rofi ~/.config/
+ln -s ~/dots/wlogout ~/.config/
+ln -s ~/dots/gammastep ~/.config/
+ln -s ~/dots/gtk-3.0 ~/.config/
+ln -s ~/dots/gtk-4.0 ~/.config/
+ln -s ~/dots/gtkrc-2.0 ~/.config/
+ln -s ~/dots/gtkrc ~/.config/
+ln -s ~/dots/konsolerc ~/.config/
+ln -s ~/dots/swappy ~/.config/
+
