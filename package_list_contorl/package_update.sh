38f7aee2a71a95519268d2293aece0cf387cb2d1
Sahil <118348625+Sahil-958@users.noreply.github.com>
2023-12-28 19:15:31 +0530

    hooks for package contorl

---
+++ package_list_contorl/package_update.sh
@@ -0,0 +1,6 @@
+pacman -Qem > /etc/pacman.d/hooks/pkglist_forgien_hooks_generated.txt
+pacman -Qe > /etc/pacman.d/hooks/pkglist_hooks_generated.txt
+
+git -C /home/sawhill/dots add pkglist_forgien_hooks_generated.txt pkglist_hooks_generated.txt
+git -C /home/sawhill/dots commit -m "PACMAN HOOKS: Updated package list to relect new changes"
+git -C /home/sawhill/dots push
