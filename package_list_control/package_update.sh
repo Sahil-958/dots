ce7dcd292c69e00cf5496e0158ebbc3c0bee03c3
Sahil <118348625+Sahil-958@users.noreply.github.com>
2023-12-29 11:06:12 +0530

    fix: permissons issues and automated commit msg

--- package_list_control/package_update.sh
+++ package_list_control/package_update.sh
@@ -1,6 +1,5 @@
 pacman -Qem > /etc/pacman.d/hooks/pkglist_forgien_hooks_generated.txt
 pacman -Qe > /etc/pacman.d/hooks/pkglist_hooks_generated.txt
 
-git -C /home/sawhill/dots add package_list_control/pkglist_forgien_hooks_generated.txt package_list_control/pkglist_hooks_generated.txt && git -C /home/sawhill/dots commit -m "PACMAN HOOKS: Updated package list to relect new changes" && git -C /home/sawhill/dots push
-
+sudo -u sawhill git -C /home/sawhill/dots add package_list_control/pkglist_forgien_hooks_generated.txt package_list_control/pkglist_hooks_generated.txt && sudo -u sawhill git -C /home/sawhill/dots commit -m "PACMAN HOOKS: Updated package list" && sudo -u sawhill git -C /home/sawhill/dots push
 
