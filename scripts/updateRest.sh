ac4fb58c957f254736af0d6322c5b08d757c108e
Sahil <118348625+Sahil-958@users.noreply.github.com>
2023-12-30 18:03:58 +0530

    dunst: using pywal generated symlink removed:themeix

--- scripts/updateRest.sh
+++ scripts/updateRest.sh
@@ -1,11 +1,12 @@
 #UPDATE DUNST COLORS AND RESTART
 
-dunst=$(themix-base16-cli /opt/oomox/plugins/base16/templates/dunst/templates/custom.mustache /opt/oomox/plugins/base16/schemes/py/py.yaml |grep -Ev "ERROR|Image plugin") 
-echo "$dunst" > ~/dots/dunst/pydunstrc
-killall dunst; cat ~/dots/dunst/dunstrc ~/dots/dunst/pydunstrc |dunst -conf - &
+#dunst=$(themix-base16-cli /opt/oomox/plugins/base16/templates/dunst/templates/custom.mustache /opt/oomox/plugins/base16/schemes/py/py.yaml |grep -Ev "ERROR|Image plugin") 
+#echo "$dunst" > ~/dots/dunst/pydunstrc
+#killall dunst; cat ~/dots/dunst/dunstrc ~/dots/dunst/pydunstrc |dunst -conf - &
 
 
 #ROFI THEME COLORS UPDATES
-themix-base16-cli /opt/oomox/plugins/base16/templates/rofi/templates/custom.mustache /opt/oomox/plugins/base16/schemes/py/py.yaml |grep -Ev "ERROR|Image plugin" > ~/dots/rofi/pywal.rasi
+#themix-base16-cli /opt/oomox/plugins/base16/templates/rofi/templates/custom.mustache /opt/oomox/plugins/base16/schemes/py/py.yaml |grep -Ev "ERROR|Image plugin" > ~/dots/rofi/pywal.rasi
 
-
+#NOT USING THEMEIX BASE 16 USING PYWAL TEMPLATES INSTEAD
+killall dunst; dunst &
