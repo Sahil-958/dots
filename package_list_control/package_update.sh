2c101d671187f9e67ab85bf792cc2927083db8a8
Sahil <118348625+Sahil-958@users.noreply.github.com>
2024-01-13 13:40:33 +0530

    -q quite flag and -n native flag added

--- package_list_control/package_update.sh
+++ package_list_control/package_update.sh
@@ -1,5 +1,5 @@
-pacman -Qem > /etc/pacman.d/hooks/pkglist_forgien_hooks_generated.txt
-pacman -Qe > /etc/pacman.d/hooks/pkglist_hooks_generated.txt
+pacman -Qqem > /etc/pacman.d/hooks/pkglist_forgien_hooks_generated.txt
+pacman -Qqen > /etc/pacman.d/hooks/pkglist_hooks_generated.txt
 
 sudo -u sawhill git -C /home/sawhill/dots add package_list_control/pkglist_forgien_hooks_generated.txt package_list_control/pkglist_hooks_generated.txt && sudo -u sawhill git -C /home/sawhill/dots commit -m "PACMAN HOOKS: Updated package list" && sudo -u sawhill git -C /home/sawhill/dots push
 
