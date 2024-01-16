03bb7db7725b52aa07303ba3168f88eb885c0fba
Sahil <118348625+Sahil-958@users.noreply.github.com>
2024-01-16 19:27:36 +0530

    removed minor bugs + toggle error on exit

--- install.sh
+++ install.sh
@@ -67,12 +67,14 @@ echo "Set root password"
 passwd
 pacman --noconfirm -S grub efibootmgr os-prober
 mkdir -p /boot/EFI
-grub-install --target=x86_64-efi --efi-directory=/boot/EFI --bootloader-id=GRUB
+grub-install --target=x86_64-efi --efi-directory=/boot/EFI --bootloader-id=$hostname
 sed -i 's/quiet/pci=noaer/g' /etc/default/grub
 grub-mkconfig -o /boot/grub/grub.cfg
 
 curl -fsSL https://raw.githubusercontent.com/Sahil-958/dots/main/package_list_control/pkglist_hooks_generated.txt > pkglist.txt ||echo "amd-ucode ark base base-devel bash-completion blueman bluez-utils breeze breeze-gtk breeze-icons brightnessctl btop calibre cliphist cmatrix cmus dmenu dolphin dolphin-plugins dunst efibootmgr ffmpegthumbs firefox firewalld fzf gammastep git glava grim grub grub-btrfs gwenview hyprland imagemagick imlib2 jq konsole linux linux-firmware man-db mesa-utils neofetch neovim nethogs network-manager-applet networkmanager noto-fonts-emoji os-prober pavucontrol pipewire-pulse polkit-kde-agent python-pyqt5 python-pywal python-qrencode qbittorrent qpdf qt5-graphicaleffects qt5-quickcontrols qt5-quickcontrols2 qt5ct qt6-wayland qt6ct reflector rofi sddm slurp snapper starship swappy sxiv terminus-font tldr tree ttf-fira-sans unzip vi vim viu vlc waybar wl-clipboard xautolock xclip xdg-desktop-portal-hyprland xf86-video-amdgpu xf86-video-ati xf86-video-nouveau xorg-server xorg-xbacklight xorg-xev xorg-xinit xorg-xkill xorg-xprop xorg-xsetroot yt-dlp zip" > pkglist.txt
 
+
+sed -i '/grub-btrfs/d' pkglist.txt
 pacman -S --noconfirm --needed - < pkglist.txt
 
 echo "%wheel ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
@@ -85,17 +87,8 @@ ai3_path=/home/$username/arch_install3.sh
 sed '1,/^#part3$/d' arch_install2.sh > $ai3_path
 chown $username:$username $ai3_path
 chmod +x $ai3_path
-sudo systemctl enable NetworkManager.service
-echo  "Rebooting in 3.."
-sleep 5
-echo -n "2.."
-sleep 5
-echo -n "1"
-sleep 5
-echo "enter disk to unmount"
-read disk
-umount -R $disk
-reboot
+echo  "Sleeping 50sec so you can exit by pressing CTRL-C and reboot"
+sleep 50
 
 #part3
 set -x
@@ -117,17 +110,20 @@ makepkg -fsri
 cd
 
 curl -fsSL https://raw.githubusercontent.com/Sahil-958/dots/main/package_list_control/pkglist_forgien_hooks_generated.txt > foregien_pkgs.txt ||echo "librewolf-bin oomox-qt5-styleplugin-git pikaur swaylock-effects-git swww themix-full-git vscodium-bin wlogout" > foregien_pkgs.txt
+sed -i '/pikaur/d' foregien_pkgs.txt
+pkgs=$(tr '\n' ' ' < foregien_pkgs.txt)
 
-#pkgs=$(cat foregien_pkgs.txt)
-
-pikaur -S swww  themeix-full-git
+set +e
+pikaur -S $pkgs
 cd
+set -e
 mkdir -p Desktop Downloads Documents Music Pictures Videos .local/share
 git clone --depth=1 https://github.com/Sahil-958/walls.git 
 git clone --depth=1 https://github.com/Sahil-958/apexNotificationSounds.git  ~/Music/apexNotificationSounds
 
 echo "making symlinks"
 cd dots
+set +e
 sudo ln -sf ~/dots/wal ~/.config/
 ~/dots/hypr/scripts/wallpaper.sh random
 sudo ln -sf ~/dots/package_list_control/packgae_update.sh /usr/bin
@@ -158,7 +154,7 @@ sudo ln -sf ~/dots/swappy ~/.config/
 sudo ln -sf ~/dots/swaylock ~/.config/
 sudo mkdir -p /etc/udev/rules.d
 sudo ln -sf ~/dots/powerNotify/99-bat.rules /etc/udev/rules.d/
-sudo ln -sf ~/.cache/wal/pywal.colorscheme ~/dots/konsole
+sudo ln -sf ~/.cache/wal/colors-konsole.colorscheme ~/dots/konsole/pywal.colorscheme
 sudo ln -sf ~/dots/konsolerc ~/.config/
 sudo ln -sf ~/dots/konsole ~/.local/share/
 sudo cp -r ~/dots/fonts /usr/share/fonts
@@ -170,7 +166,12 @@ sudo ln -sf ~/dots/.gitconfig ~/
 sudo ln -sf ~/dots/.vimrc ~/
 sudo ln -sf ~/dots/qt5ct ~/.config/
 sudo ln -sf ~/dots/qt6ct ~/.config/
+rm -rf ~/.icons
 sudo cp -r ~/dots/.icons ~/  
+echo "
+[Icon Theme]
+Inherits=breeze_cursors
+" | sudo tee /usr/share/icons/default/index.theme
 sudo cp ~/dots/environment /etc/
 sudo cp ~/dots/grub /etc/default/
 sudo grub-mkconfig -o /boot/grub/grub.cfg
