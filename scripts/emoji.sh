af2ca6a41bb5e5b5d41d50a95ec39ffd8d287280
Sahil <118348625+Sahil-958@users.noreply.github.com>
2024-01-28 15:56:28 +0530

    added emoji picker

---
+++ scripts/emoji.sh
@@ -0,0 +1,4 @@
+rofi -modi emoji -show emoji -kb-custom-1 Ctrl+c -p "Copy"  -config "~/dots/rofi/config-dmenu.rasi" -theme-str 'entry { placeholder: "E.g. Nerd";}
+listview { columns: 2;}
+window{height: 40%; width: 50%;}' | wl-copy
+
