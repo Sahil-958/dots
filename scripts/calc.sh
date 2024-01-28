686eeb3e719b166092a7778e0a3ca07c0ac3410f
Sahil <118348625+Sahil-958@users.noreply.github.com>
2024-01-28 15:57:17 +0530

    CHNG: welcome text in message box

--- scripts/calc.sh
+++ scripts/calc.sh
@@ -1,2 +1,2 @@
-rofi -show calc -modi calc -no-show-match -no-sort -config "~/dots/rofi/config-dmenu.rasi" -theme-str 'entry { placeholder: "E.g. 5 dollars to INR";}
+rofi -show calc -modi calc  -hint-welcome "Waiting for Input..." -no-show-match -no-sort -automatic-save-to-history -config "~/dots/rofi/config-dmenu.rasi" -theme-str 'entry { placeholder: "E.g. 4 dollars to INR";}
 window{height: 50%; width: 50%;}' | wl-copy
