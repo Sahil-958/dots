f0b4d2daadcfc2b9e7ef602c849582f2039a8185
Sahil <118348625+Sahil-958@users.noreply.github.com>
2024-01-28 15:35:53 +0530

    CHNG: added message to listbox and script to call rofi calc

---
+++ scripts/calc.sh
@@ -0,0 +1,2 @@
+rofi -show calc -modi calc -no-show-match -no-sort -config "~/dots/rofi/config-dmenu.rasi" -theme-str 'entry { placeholder: "E.g. 5 dollars to INR";}
+window{height: 50%; width: 50%;}' | wl-copy
