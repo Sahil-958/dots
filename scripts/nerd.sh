e208dec83557401f31f695808dedcedd585a674b
Sahil <118348625+Sahil-958@users.noreply.github.com>
2024-02-05 20:53:55 +0530

    using better approch with update option

--- scripts/nerd.sh
+++ scripts/nerd.sh
@@ -1,4 +1,12 @@
 #!/bin/sh
-cat ~/dots/scripts/chars.txt|sort -u|rofi -dmenu -i -matching fuzzy -config ~/dots/rofi/config-dmenu.rasi -theme-str '
-*{font: "ComicShannsMono Nerd Font 18";} 
-listview{columns: 3; fixed-columns: false;} window{ width: 70%; height: 40%;}' |sed 's/^\(.\).*/\1/'|tr -d '\n'|wl-copy
+
+case $1 in
+    update)
+        curl -fsSL https://raw.githubusercontent.com/ryanoasis/nerd-fonts/master/glyphnames.json > ~/dots/scripts/glyphnames.json && echo "glyphnames.json updated"
+        ;;
+    *)
+        jq -r 'to_entries[] | "\(.value.char)   \(.key)"' ~/dots/scripts/glyphnames.json |rofi -dmenu -i -matching fuzzy -config ~/dots/rofi/config-dmenu.rasi -theme-str '
+        *{font: "ComicShannsMono Nerd Font 18";} 
+        listview{columns: 3; fixed-columns: false;} window{ width: 70%; height: 40%;}' |sed 's/^\(.\).*/\1/'|tr -d '\n'|wl-copy
+        ;;
+esac
