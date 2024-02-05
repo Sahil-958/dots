bbdd8ae5c0fa5117b9db12e8cc872ce74c9e97de
Sahil <118348625+Sahil-958@users.noreply.github.com>
2024-02-05 20:19:46 +0530

    added case insensitive search & fuzzy matching

--- scripts/nerd.sh
+++ scripts/nerd.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-cat ~/dots/scripts/chars.txt  |rofi -dmenu -config ~/dots/rofi/config-dmenu.rasi -theme-str '
+cat ~/dots/scripts/chars.txt  |rofi -dmenu -i -matching fuzzy -config ~/dots/rofi/config-dmenu.rasi -theme-str '
 *{font: "ComicShannsMono Nerd Font 18";} 
 listview{columns: 3; fixed-columns: false;} window{ width: 70%; height: 40%;}' |sed 's/^\(.\).*/\1/'|tr -d '\n'|wl-copy
