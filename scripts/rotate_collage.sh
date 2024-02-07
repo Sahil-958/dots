94dcc45c4f41f835f32c2d1d036c79ea601c75f6
Sahil <118348625+Sahil-958@users.noreply.github.com>
2024-02-07 21:56:58 +0530

    added -h flag

--- scripts/rotate_collage.sh
+++ scripts/rotate_collage.sh
@@ -1,5 +1,28 @@
 #!/bin/bash
 
+help_message="Imagemagik is required for this scirpt to work
+Usage: $(basename "$0") path/to/image1.png ... path/to/imageN.png <angle of join>
+
+For Example: 
+    let current dir contains: img1.png img2.png img3.png
+    then you can use it as:
+    $(basename "$0") img1.png img2.png img3.png 10
+
+This will create an output.png in your current dir which contains all images combined at angle 10
+The <angle> at last is nessary if you want only horizontal join pass 0
+"
+
+display_help() {
+    echo "$help_message"
+}
+
+# Check if -h option is passed
+if [[ "$1" == "-h" ]]; then
+    display_help
+    exit 0
+fi
+
+
 # Calculate the height of each cropped part
 images=$(($# - 1))
 
