89c162cea9e141fd639f33fae35b0c4ff6cdd94e
Sahil <118348625+Sahil-958@users.noreply.github.com>
2024-02-12 16:56:12 +0530

    added cache capablity

--- scripts/genQTGTK.sh
+++ scripts/genQTGTK.sh
@@ -5,20 +5,35 @@ suru="suruplus"
 suru_aspromauros="suruplus_aspromauros"
 none="Do not Generate Icons"
 
-choice=$(printf "$numix\n$suru\n$suru_aspromauros\n$papirus\n$none" | rofi -dmenu -p "Select Icon Theme" -config ~/dots/rofi/config-dmenu.rasi) 
+source ~/.cache/wal/colors.sh
+icon_dir="/home/sawhill/.cache/icon_theme_cache"
+mkdir -p $icon_dir
+wall=${wallpaper//[\/.]/_}
+icon_dir+="/$wall"
+#cp "~/.icons/testing_cli" "$icon_dir"
+echo "$icon_dir"
+if [ -d "$icon_dir" ]; then
+    echo "exist"
+    ln -sf "$icon_dir" ~/.icons/testing_cli
+    notify-send "Updated Icon Theme ColorScheme"
+    exit 0
+fi
 
+choice=$(printf "$numix\n$suru\n$suru_aspromauros\n$papirus\n$none" | rofi -dmenu -p "Select Icon Theme" -config ~/dots/rofi/config-dmenu.rasi) 
 
 if [ "$choice" != "$none" ] ; then
 #	showNotification $line_count &
 run_script="/opt/oomox/plugins/icons_$choice/change_color.sh" 
 
-$run_script -o testing_cli -d ~/.icons/testing_cli ~/.cache/wal/colors-oomox > >(while read -r line; do
+$run_script -o testing_cli -d "$icon_dir" ~/.cache/wal/colors-oomox > >(while read -r line; do
         chk=$((chk +$(echo $line|wc -l)))
         progress=$(( (chk - 1) * 100 / 5  ))
 	dunstify -t 100000 -h int:value:$progress "Generating Icons Colorscheme..." 
 done)
 
-dunstify   "Icon ColorScheme Generation Completed" 
+ln -sf "$icon_dir" ~/.icons/testing_cli
+
+dunstify "Icon ColorScheme Generation Completed" 
 sleep 2
 dunstctl close
 
