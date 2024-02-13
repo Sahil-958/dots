5484d693fddea7c257af218864451c3744fb3221
Sahil <118348625+Sahil-958@users.noreply.github.com>
2024-02-13 18:40:55 +0530

    using indexs for dmenu output

--- scripts/powermenu.sh
+++ scripts/powermenu.sh
@@ -6,32 +6,31 @@ prompt="Don't forget to touch some Grass!"
 message="$uptime | Updates Available: $updates"
 # Prompt user to select an action using rofi
 # echo -en "testing\0icon\x1f<span color='@foreground'>⏻</span>" | ./rofi -dmenu
-action=$(echo ' 
+action=$(echo '
 
 󰍃
-' | rofi -dmenu -kb-select-1 's' -kb-select-2 'r' -kb-select-3 'e' -kb-select-4 'l' -i -matching regex -config "~/dots/rofi/config-power.rasi" -p "$prompt" -mesg "$message" -theme-str '
+' | rofi -dmenu -format 'd' -kb-select-1 's' -kb-select-2 'r' -kb-select-3 'e' -kb-select-4 'l' -i -matching regex -config "~/dots/rofi/config-power.rasi" -p "$prompt" -mesg "$message" -theme-str '
 window { fullscreen: true; }
 ')
 
-
 # Check the selected action and run the appropriate command
 case $action in
-    "")
+    "1")
         # Run shutdown command
         sleep 0.5s
         systemctl poweroff 
         ;;
-    "")
+    "2")
         # Run reboot command
         sleep 0.5s
         systemctl reboot
         ;;
-    "󰍃")
+    "3")
         # Run logout command (replace with your preferred logout command)
         sleep 0.5s
         hyprctl dispatch exit
         ;;
-    "")
+    "4")
         # Run lock command (replace with your preferred lock command)
         sleep 0.5s
         swaylock 
