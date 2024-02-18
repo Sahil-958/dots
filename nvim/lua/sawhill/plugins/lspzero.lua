2a0ba85e43e36da003540dd53ae2ee9ce7376411
Sahil <118348625+Sahil-958@users.noreply.github.com>
2024-02-18 16:31:11 +0530

    fix: tab keybinding, prev_item, next_item keybinds

--- nvim/lua/sawhill/plugins/lspzero.lua
+++ nvim/lua/sawhill/plugins/lspzero.lua
@@ -58,15 +58,23 @@ return {
                 },
                 mapping = {
                     ['<C-Space>'] = cmp.mapping.complete(),
-                    ['<C-p>'] = cmp.mapping.select_prev_item({behavior = 'select'}),
-                    ['<C-n>'] = cmp.mapping.select_next_item({behavior = 'select'}),
+                    ['<C-p>'] = cmp.mapping.select_prev_item({ behavior = 'select' }),
+                    ['<C-n>'] = cmp.mapping.select_next_item({ behavior = 'select' }),
                     ['<C-y>'] = cmp.mapping.confirm({ select = true }),
                     ['<CR>'] = cmp.mapping.confirm({ select = true }),
                     ['<C-u>'] = cmp.mapping.scroll_docs(-4),
                     ['<C-d>'] = cmp.mapping.scroll_docs(4),
                     ['<C-f>'] = cmp_action.luasnip_jump_forward(),
                     ['<C-b>'] = cmp_action.luasnip_jump_backward(),
-                    ['<Tab>'] = cmp_action.tab_complete(),
+                    ['<Tab>'] = cmp.mapping(function()
+                        if cmp.visible() then
+                            cmp.select_next_item({ behavior = 'select' })
+                            --cmp_action.tab_complete()
+                        else
+                            local tabstop = vim.opt.tabstop:get()
+                            vim.cmd("normal! i" .. string.rep(" ", tabstop))
+                        end
+                    end),
                     ['<S-Tab>'] = cmp_action.select_prev_or_fallback(),
                 },
                 window = {
@@ -100,60 +108,47 @@ return {
                 lsp_zero.default_keymaps({ buffer = bufnr })
                 vim.keymap.set("n", "<leader>f", function() vim.lsp.buf.format() end, opts)
                 --[[
-	DEFAULTS
-	K:
-	Displays hover information about the symbol under the cursor in a
-	floating window. See |vim.lsp.buf.hover()|.
+                    DEFAULTS
+                    K:
+                    Displays hover information about the symbol under the cursor in a floating window. See |vim.lsp.buf.hover()|.
 
-	gd:
-	Jumps to the definition of the symbol under the cursor.
-	See |vim.lsp.buf.definition()|.
+                    gd:
+                    Jumps to the definition of the symbol under the cursor. See |vim.lsp.buf.definition()|.
 
-	gD:
-	Jumps to the declaration of the symbol under the cursor. Some servers
-	don't implement this feature. See |vim.lsp.buf.declaration()|.
+                    gD:
+                    Jumps to the declaration of the symbol under the cursor. Some servers don't implement this feature. See |vim.lsp.buf.declaration()|.
 
-	gi:
-	Lists all the implementations for the symbol under the cursor in the
-	quickfix window. See |vim.lsp.buf.implementation()|.
+                    gi:
+                    Lists all the implementations for the symbol under the cursor in the quickfix window. See |vim.lsp.buf.implementation()|.
 
-	go:
-	Jumps to the definition of the type of the symbol under the cursor.
-	See |vim.lsp.buf.type_definition()|.
+                    go:
+                    Jumps to the definition of the type of the symbol under the cursor. See |vim.lsp.buf.type_definition()|.
 
-	gr:
-	Lists all the references to the symbol under the cursor in the quickfix
-	window. See |vim.lsp.buf.references()|.
+                    gr:
+                    Lists all the references to the symbol under the cursor in the quickfix window. See |vim.lsp.buf.references()|.
 
-	gs:
-	Displays signature information about the symbol under the cursor in a
-	floating window. See |vim.lsp.buf.signature_help()|. If a mapping
-	already exists for this key this function is not bound.
+                    gs:
+                    Displays signature information about the symbol under the cursor in a floating window. See |vim.lsp.buf.signature_help()|. If a mapping already exists for this key this function is not bound.
 
-	<F2>:
-	Renames all references to the symbol under the cursor.
-	See |vim.lsp.buf.rename()|.
+                    <F2>:
+                    Renames all references to the symbol under the cursor. See |vim.lsp.buf.rename()|.
 
-	<F3>:
-	Format a buffer using the LSP servers attached to it.
-	See |vim.lsp.buf.format()|.
+                    <F3>:
+                    Format a buffer using the LSP servers attached to it. See |vim.lsp.buf.format()|.
 
-	<F4>:
-	Selects a code action available at the current cursor position.
-	See |vim.lsp.buf.code_action()|.
+                    <F4>:
+                    Selects a code action available at the current cursor position. See |vim.lsp.buf.code_action()|.
 
-	gl:
-	Show diagnostic in a floating window. See |vim.diagnostic.open_float()|.
+                    gl:
+                    Show diagnostic in a floating window. See |vim.diagnostic.open_float()|.
 
-	[d:
-	Move to the previous diagnostic in the current buffer.
-	See |vim.diagnostic.goto_prev()|.
+                    [d:
+                    Move to the previous diagnostic in the current buffer. See |vim.diagnostic.goto_prev()|.
 
-	]d:
-	Move to the next diagnostic. See |vim.diagnostic.goto_next()|.
+                    ]d:
+                    Move to the next diagnostic. See |vim.diagnostic.goto_next()|.
 
-
-	]]
+                ]]
                 --
             end)
 
