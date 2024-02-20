b7fe8c2da5ff9625dfd173d5ac950c34020cf0f8
Sahil <118348625+Sahil-958@users.noreply.github.com>
2024-02-20 19:06:16 +0530

    using bc for subtraction and comments removed

--- powerNotify/chrgnotify.sh
+++ powerNotify/chrgnotify.sh
@@ -1,38 +1,21 @@
 #!/bin/sh
-set -e
-# I'm keeping the sudo invocation of notify-send that have addtition uneeded options specified  but only the last one has minimal needed options for working as intended 
 
 function notify(){
-    #Detect the name of the display in use
-    #display=":$(ls /tmp/.X11-unix/* | sed 's#/tmp/.X11-unix/X##' | head -n 1)"
-    #way0="wayland-0"
-    #way1="wayland-1" 
-    #Detect the user using such display
-    #user=$(who | awk '{print $1}' | head -n 1)
-    #Detect the id of the user
-    #uid=$(id -u $user)
-    #sudo -u $user DISPLAY=$display DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/$uid/bus /usr/bin/notify-send "$@"
-    #sudo -u $user WAYLAND_DISPLAY=$way0 DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/$uid/bus /usr/bin/notify-send "$@"
-    #sudo -u $user WAYLAND_DISPLAY=$way1 DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/$uid/bus /usr/bin/notify-send "$@"
-    #echo "$display $way0 $way1 $user $uid $@"
-    #DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/$uid/bus  /usr/bin/notify-send "$@"
-    # Get some random icon
+   # Get some random icon
     icon=$(find ~/.icons/ -name "*adapter*"|shuf -n 1)
 
-
-    #only needed portion of the function
     user=$(who | awk '{print $1}' | head -n 1)
     uid=$(id -u $user)
     DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/$uid/bus  /usr/bin/notify-send -i "$icon" "$@"
 }
 
 function stats(){
-       time=$(cat /tmp/chargeLog 2>/dev/null | head -n 1 || echo "can't retrieve status")
+       time=$(cat ~/.cache/chargeLog 2>/dev/null | head -n 1 || echo "can't retrieve status")
        currentTime=$(date +%s)
-       preCharg=$(cat /tmp/chargeLog 2>/dev/null | tail -n 1 || echo "can't retrieve status")
-       chargDiff="$(($per - $preCharg ))"
-       timeDiff=$(("$currentTime" - $time))
-       formattedTime=$(date -d "@$timeDiff" -u +%H:%M:%S)
+       preCharg=$(cat ~/.cache/chargeLog 2>/dev/null | tail -n 1 || echo "can't retrieve status")
+       chargDiff=$(echo "$per - $preCharg" | bc )
+       timeDiff=$(echo "$currentTime - $time" | bc )
+       formattedTime=$(date -d "@$timeDiff" -u +%H:%M:%S 2>/dev/null || echo "Error")
 }
 
 chargDiff=""
@@ -43,12 +26,15 @@ case $1 in
     plugged) 
              stats
              notify "Charger Plugged" "Dischargred $chargDiff% in $formattedTime"
-             echo -e "$(date +%s)\n$per" > /tmp/chargeLog
+             echo -e "$(date +%s)\n$per" > ~/.cache/chargeLog
     ;;
     unplugged) 
                stats
                notify "Charger Unplugged" "Chargred $chargDiff% in $formattedTime"
-               echo -e "$(date +%s)\n$per" > /tmp/chargeLog
+               echo -e "$(date +%s)\n$per" > ~/.cache/chargeLog
+    ;;
+    init)
+             echo -e "$(date +%s)\n$per" > ~/.cache/chargeLog
     ;;
     *) notify ">|$1|<"
     ;;
