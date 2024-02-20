c2a549844d245dac6df212d4d5ffb0ce7f6aac33
Sahil <118348625+Sahil-958@users.noreply.github.com>
2024-02-20 17:51:48 +0530

    added config for Mason window

--- nvim/lua/sawhill/plugins/lspzero.lua
+++ nvim/lua/sawhill/plugins/lspzero.lua
@@ -13,7 +13,31 @@ return {
     {
         'williamboman/mason.nvim',
         lazy = false,
-        config = true,
+        config = function()
+            require('mason').setup({
+                ui = {
+                    ---@since 1.0.0
+                    -- Whether to automatically check for new versions when opening the :Mason window.
+                    check_outdated_packages_on_open = true,
+
+                    ---@since 1.0.0
+                    -- The border to use for the UI window. Accepts same border values as |nvim_open_win()|.
+                    border = "double",
+
+                    ---@since 1.0.0
+                    -- Width of the window. Accepts:
+                    -- - Integer greater than 1 for fixed width.
+                    -- - Float in the range of 0-1 for a percentage of screen width.
+                    width = 0.8,
+
+                    ---@since 1.0.0
+                    -- Height of the window. Accepts:
+                    -- - Integer greater than 1 for fixed height.
+                    -- - Float in the range of 0-1 for a percentage of screen height.
+                    height = 0.9,
+                },
+            })
+        end,
     },
 
     -- Autocompletion
