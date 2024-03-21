f23ac515872d6ec496768b3c072ef03e141cd84c
Sahil <118348625+Sahil-958@users.noreply.github.com>
2024-03-21 12:17:17 +0530

    maintainance changes: removed unnecessary items

--- scripts/updateRest.sh
+++ scripts/updateRest.sh
@@ -48,25 +48,3 @@ pgrep glava && killall glava && glava
 # -----------------------------------------------------
 #~/dots/scripts/swaylock.sh
 
-#or just use sed as below
-
-#source /home/sawhill/.cache/wal/colors-swaylock.sh
-# Update swaylock config colors and wallpaper
-#sed -i "s|image=.*$|image=$wallpaper|g" ~/.config/swaylock/config
-#sed -i "s|ring-color=.*$|ring-color=$foreground|g" ~/.config/swaylock/config
-#sed -i "s|ring-clear-color=.*$|ring-clear-color=$foreground|g" ~/.config/swaylock/config
-#sed -i "s|ring-ver-color=.*$|ring-ver-color=$foreground|g" ~/.config/swaylock/config
-#sed -i "s|ring-wrong-color=.*$|ring-wrong-color=$foreground|g" ~/.config/swaylock/config
-#
-#sed -i "s|line-caps-color=.*$|line-caps-color=${foreground}A0|g" ~/.config/swaylock/config
-#sed -i "s|line-clear-color=.*$|line-clear-color=${foreground}A0|g" ~/.config/swaylock/config
-#sed -i "s|line-ver-color=.*$|line-ver-color=${foreground}A0|g" ~/.config/swaylock/config
-#sed -i "s|line-wrong-color=.*$|line-wrong-color=${foreground}A0|g" ~/.config/swaylock/config
-#
-#sed -i "s|text-color=.*$|text-color=${foreground}|g" ~/.config/swaylock/config
-#sed -i "s|text-clear-color=.*$|text-clear-color=${foreground}|g" ~/.config/swaylock/config
-#sed -i "s|text-ver-color=.*$|text-ver-color=${foreground}|g" ~/.config/swaylock/config
-#sed -i "s|text-wrong-color=.*$|text-wrong-color=${foreground}|g" ~/.config/swaylock/config
-#
-#sed -i "s|bs-hl-color=.*$|bs-hl-color=${foreground}|g" ~/.config/swaylock/config
-#sed -i "s|text-caps-lock-color=.*$|text-caps-lock-color=${foreground}|g" ~/.config/swaylock/config
