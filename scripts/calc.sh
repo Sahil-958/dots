4fc97b723df3abb8d97ff88a4fa3ad87adfd3c4a
Sahil <118348625+Sahil-958@users.noreply.github.com>
2024-03-26 12:47:13 +0530

    fix: rofi config path

--- scripts/calc.sh
+++ scripts/calc.sh
@@ -1,2 +1,2 @@
-rofi -show calc -modi calc  -hint-welcome "Waiting for Input..." -selected-row 0 -no-show-match -no-sort -automatic-save-to-history -config ~/dots/rofi/config-dmenu.rasi -theme-str 'entry { placeholder: "E.g. 4 dollars to INR";}
+rofi -show calc -modi calc  -hint-welcome "Waiting for Input..." -selected-row 0 -no-show-match -no-sort -automatic-save-to-history -config ~/dots/config/rofi/config-dmenu.rasi -theme-str 'entry { placeholder: "E.g. 4 dollars to INR";}
 window{height: 50%; width: 50%;}' | wl-copy
