1ae319b0244818df3ac18c1def5167b9796414a4
Sahil <118348625+Sahil-958@users.noreply.github.com>
2024-03-28 15:30:52 +0530

    removed custom keybind arg

--- scripts/emoji.sh
+++ scripts/emoji.sh
@@ -1,4 +1,4 @@
-rofi -modi emoji -show emoji -kb-custom-1 Ctrl+c -p "Copy"  -config "~/dots/config/rofi/config-dmenu.rasi" -theme-str 'entry { placeholder: "E.g. Nerd";}
+rofi -modi emoji -show emoji -p "Copy"  -config "~/dots/config/rofi/config-dmenu.rasi" -theme-str 'entry { placeholder: "E.g. Nerd";}
 listview { columns: 2;}
 window{height: 40%; width: 50%;}' | wl-copy
 
