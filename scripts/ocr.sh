c1122e3bd0378ff60607a9bab06625a70d1564c1
Sahil <118348625+Sahil-958@users.noreply.github.com>
2024-05-02 15:07:25 +0530

    show OCRed text in notification body

--- scripts/ocr.sh
+++ scripts/ocr.sh
@@ -4,4 +4,13 @@ source ~/.cache/wal/colors.sh
 
 #grim -t png -g "$(slurp -b "${background}90" -c "${foreground}")" - | tesseract stdin stdout | wl-copy && notify-send "Selection Copied into the clipborad"
 # Use below if don't want notify-send to run when you don't selecte anything
-selection="$(slurp -b "${background}90" -c "${foreground}")" && grim -t png -g "$selection" - | tesseract stdin stdout | wl-copy && notify-send "Selection Copied into the clipboard"
+#selection="$(slurp -b "${background}90" -c "$foreground")" && grim -t png -g "$selection" - | tesseract stdin stdout | wl-copy && notify-send "Selection Copied into the clipboard"
+#
+#
+selection="$(slurp -b "${background}90" -c "$foreground")" 
+text=$(grim -t png -g "$selection" - | tesseract stdin stdout)
+if [ "$text" = "" ]; then
+    notify-send "No text detected" "Please select a valid text"
+    exit 1
+fi
+wl-copy "$text" && notify-send "Selection Copied into the clipboard" "$text"
