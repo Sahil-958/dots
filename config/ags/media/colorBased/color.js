8f96d28ca7d5259dd156689c1dd1191165a1a0ef
Sahil <118348625+Sahil-958@users.noreply.github.com>
2024-06-28 11:33:28 +0530

    preparing media widget for lockscreen

---
+++ config/ags/media/colorBased/color.js
@@ -0,0 +1,185 @@
+
+export function nearestNeighbor(pixels, width, height, factor) {
+    const newWidth = Math.floor(width / factor);
+    const newHeight = Math.floor(height / factor);
+    const newPixels = [];
+
+    for (let y = 0; y < newHeight; y++) {
+        for (let x = 0; x < newWidth; x++) {
+            const i = y * newWidth + x;
+            const j = Math.floor(y * factor) * width + Math.floor(x * factor)
+            newPixels[i] = pixels[j];
+        }
+    }
+
+    return newPixels;
+}
+
+export function quantizePixels(pixels, numColors, iterationCount = 1) {
+    // Helper function to calculate the Euclidean distance between two pixels
+    function distance(p1, p2) {
+        return Math.sqrt((p1[0] - p2[0]) ** 2 + (p1[1] - p2[1]) ** 2 + (p1[2] - p2[2]) ** 2)
+    }
+
+    // Initialize centroids randomly
+    const centroids = [];
+    for (let i = 0; i < numColors; i++) {
+        const randomIndex = Math.floor(Math.random() * pixels.length);
+        centroids.push(pixels[randomIndex]);
+    }
+
+    let clusters = new Array(numColors).fill(null).map(() => []);
+    let oldClusters = [];
+
+    // Repeat until convergence
+    for (let i = 0; i < iterationCount; i++) {
+        // Assign each pixel to the nearest centroid
+        clusters = new Array(numColors).fill(null).map(() => []);
+        for (const pixel of pixels) {
+            let minDistance = Infinity;
+            let closestCentroidIndex = -1;
+            for (let i = 0; i < centroids.length; i++) {
+                const d = distance(pixel, centroids[i]);
+                if (d < minDistance) {
+                    minDistance = d;
+                    closestCentroidIndex = i;
+                }
+            }
+            clusters[closestCentroidIndex].push(pixel);
+        }
+
+        // Check for convergence
+        if (i + 1 === iterationCount) {
+            break;
+        }
+
+        // Update centroids to the mean of the clusters
+        for (let i = 0; i < numColors; i++) {
+            if (clusters[i].length > 0) {
+                const sum = clusters[i].reduce(
+                    (acc, pixel) => {
+                        return [acc[0] + pixel[0], acc[1] + pixel[1], acc[2] + pixel[2]];
+                    },
+                    [0, 0, 0],
+                );
+                centroids[i] = [
+                    Math.round(sum[0] / clusters[i].length),
+                    Math.round(sum[1] / clusters[i].length),
+                    Math.round(sum[2] / clusters[i].length),
+                ];
+            }
+        }
+
+        oldClusters = clusters;
+    }
+
+    // Replace each pixel with the centroid of the cluster it belongs to
+    const quantizedPixels = pixels.map((pixel) => {
+        let minDistance = Infinity
+        let closestCentroid = [0, 0, 0]
+        for (const centroid of centroids) {
+            const d = distance(pixel, centroid)
+            if (d < minDistance) {
+                minDistance = d
+                closestCentroid = centroid
+            }
+        }
+        return closestCentroid
+    })
+
+    return quantizedPixels
+}
+
+function calculateLuminance(r, g, b) {
+    return 0.2126 * r + 0.7152 * g + 0.0722 * b
+}
+
+function calculateVibrance(r, g, b) {
+    return Math.max(r, g, b) - Math.min(r, g, b)
+}
+
+export function selectDarkVibrantPixel(pixels) {
+    let darkVibrantPixel;
+    let maxVibrance = -1;
+    let minLuminance = 256; // Luminance can't be more than 255
+
+    for (const pixel of pixels) {
+        const [r, g, b] = pixel
+        const luminance = calculateLuminance(r, g, b)
+        const vibrance = calculateVibrance(r, g, b)
+
+        // Check if this pixel is darker and more vibrant than the current candidate
+        if (vibrance > maxVibrance && luminance < minLuminance) {
+            darkVibrantPixel = pixel
+            maxVibrance = vibrance
+            minLuminance = luminance
+        }
+    }
+
+    return { pixel: darkVibrantPixel, vibrance: maxVibrance, luminance: minLuminance }
+}
+
+/// Conversion helpers
+// All using 255 as the max value
+export function hexToRGB(hex) {
+    const r = parseInt(hex.slice(1, 3), 16)
+    const g = parseInt(hex.slice(3, 5), 16)
+    const b = parseInt(hex.slice(5, 7), 16)
+    return [r, g, b]
+}
+export function rgbToHex(rgb) {
+    const [r, g, b] = rgb
+    return `#${((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1)}`
+}
+export function rgbToHSL(rgb) {
+    const [r, g, b] = rgb.map((x) => x / 255)
+    const max = Math.max(r, g, b)
+    const min = Math.min(r, g, b)
+    let h, s, l = (max + min) / 2
+
+    if (max === min) {
+        h = s = 0 // achromatic
+    } else {
+        const d = max - min
+        s = l > 0.5 ? d / (2 - max - min) : d / (max + min)
+        switch (max) {
+            case r:
+                h = (g - b) / d + (g < b ? 6 : 0)
+                break
+            case g:
+                h = (b - r) / d + 2
+                break
+            case b:
+                h = (r - g) / d + 4
+                break
+        }
+        h /= 6
+    }
+
+    return [h * 360, s * 100, l * 100]
+}
+export function hslToRGB(hsl) {
+    const [h, s, l] = hsl.map((x, i) => (i === 0 ? x / 360 : x / 100))
+    let r, g, b;
+
+    if (s === 0) {
+        r = g = b = l // achromatic
+    } else {
+        const hue2rgb = (p, q, t) => {
+            if (t < 0) t += 1
+            if (t > 1) t -= 1
+            if (t < 1 / 6) return p + (q - p) * 6 * t
+            if (t < 1 / 2) return q
+            if (t < 2 / 3) return p + (q - p) * (2 / 3 - t) * 6
+            return p
+        }
+
+        const q = l < 0.5 ? l * (1 + s) : l + s - l * s
+        const p = 2 * l - q
+        r = hue2rgb(p, q, h + 1 / 3)
+        g = hue2rgb(p, q, h)
+        b = hue2rgb(p, q, h - 1 / 3)
+    }
+
+    return [r * 255, g * 255, b * 255]
+}
---
+++ config/ags/media/colorBased/music.js
@@ -0,0 +1,195 @@
+import {
+    hexToRGB,
+    hslToRGB,
+    rgbToHSL,
+    nearestNeighbor,
+    quantizePixels,
+    selectDarkVibrantPixel,
+} from "./color"
+import { existsSync } from './fs'
+import Gdk from 'types/@girs/gdk-3.0/gdk-3.0'
+import GdkPixbuf from 'types/@girs/gdkpixbuf-2.0/gdkpixbuf-2.0'
+
+const mpris = await Service.import('mpris')
+
+const MusicColor = Variable({ color: '#1e1e2e', variant: 'dark', imageUrl: '' })
+
+// Takes a URL, stores the file in a temporary dir, loads it into cairo and fades the left side with a solid color
+export function SongArt(player: MprisPlayer) {
+    let url = player.track_cover_url
+    let image = `/tmp/ags/song-art/${url.split('/').pop()}.jpg`
+
+    const albumArt = Widget.DrawingArea({
+        widthRequest: 32,
+        heightRequest: 32,
+        /** Draws a faded image from left to right */
+        drawFn: (_, cr, width, height) => {
+            if (!existsSync(image)) return
+
+            const pixBuf = GdkPixbuf.Pixbuf.new_from_file(image)
+            const imageSurface = Gdk.cairo_surface_create_from_pixbuf(pixBuf, 1, null)
+
+            if (MusicColor.value.imageUrl !== url) {
+                const pixels = pixBuf.get_pixels().reduce < [number, number, number][] > ((acc, val, i, pixels) => {
+                    if (i % 3 === 0) acc.push([val, pixels[i + 1], pixels[i + 2]])
+                    return acc
+                }, [])
+                const width = pixBuf.get_width()
+                const height = pixBuf.get_height()
+
+                const scaleDownFactor = Math.max(1, Math.floor(Math.max(width, height) / 200))
+                const scaledDownPixels = nearestNeighbor(pixels, width, height, scaleDownFactor)
+                const vibrantSwatches = quantizePixels(scaledDownPixels, 64)
+                const { pixel, luminance } = selectDarkVibrantPixel(vibrantSwatches)
+                const rgbToHex = (rgb: [number, number, number]) =>
+                    `#${rgb.map((c) => c.toString(16).padStart(2, '0')).join('')}`
+                MusicColor.setValue({
+                    color: rgbToHex(pixel),
+                    variant: luminance > 150 ? 'light' : 'dark',
+                    imageUrl: url,
+                })
+            }
+
+            // Calculate the scale factor to fill the image
+            const imgWidth = imageSurface.getWidth()
+            const imgHeight = imageSurface.getHeight()
+            const scaleFactor = Math.min(width / imgWidth, height / imgHeight)
+            cr.scale(scaleFactor, scaleFactor)
+
+            const borderRadius = config.theme.borderRadius
+            for (let x = 0; x < width; x++) {
+                // Calculate the y offset for the current x position to comply with the border radius
+                // on the right side only
+                const borderRadiusAmount = Math.max(0, x - width + borderRadius + 1) / borderRadius
+                // x^2 + (y - 1)^2 = 1 => y = sqrt(1 - x^2) - 1
+                // Circle transposed down to be x=0,y=0 and x=1,y=-1
+                const y = Math.sqrt(1 - borderRadiusAmount ** 2) - 1
+                const yOffset = (-y * borderRadius) / scaleFactor
+
+                // clip to the current column
+                cr.rectangle(x / scaleFactor, yOffset, 1 / scaleFactor, imgHeight - yOffset * 2)
+                cr.save()
+                cr.clip()
+
+                // paint the image
+                cr.setSourceSurface(imageSurface, 0, 0)
+                cr.paintWithAlpha(Math.min((x / width) * 2, 1))
+
+                // revert the clip
+                cr.restore()
+            }
+        },
+    }).hook(player, async (albumArt) => {
+        url = player.track_cover_url
+        image = `/tmp/ags/song-art/${url.split('/').pop()}.jpg`
+        // TODO: move logic out and deduplicate calls
+        if (!existsSync(image)) {
+            await Utils.execAsync(`mkdir -p /tmp/ags/song-art`).catch(console.error)
+            await Utils.execAsync(`curl -o ${image} ${url}`).catch(console.error)
+        }
+        albumArt.queue_draw()
+    })
+
+    return albumArt
+}
+
+const audio = await Service.import('audio')
+const changeSpotifyVolume = (raise: boolean) => {
+    // @ts-expect-error - findLast actually does exist
+    const spotifyStream = audio.apps.findLast((app: Stream) => app.stream?.name === 'spotify')
+    if (!spotifyStream) return
+
+    const newVolume = raise
+        ? Math.min(1, spotifyStream.volume + 0.05) // if raise
+        : Math.max(0, spotifyStream.volume - 0.05) // if lower
+
+    spotifyStream.volume = newVolume
+}
+
+const copySpotifyURL = (url?: string) => {
+    Utils.execAsync(`wl-copy ${url}`)
+}
+
+const Player = (player: MprisPlayer) => {
+    const artist = Widget.Label({ css: `opacity: 0.8;`, halign: Align.START }).hook(player, (label) => {
+        label.label = player.track_artists.join(', ')
+    })
+    const title = Widget.Label({ halign: Align.START }).hook(player, (label) => {
+        label.label = player.track_title
+    })
+
+    const titleAndArtist = Widget.Box({
+        valign: Align.CENTER,
+        orientation: Orientation.VERTICAL,
+        children: [title, artist],
+    })
+    const playPauseIcon = Icon(
+        player
+            .bind('play_back_status')
+            .as((playbackStatus) => (playbackStatus === 'Playing' ? 'player-pause' : 'player-play')),
+    )
+
+    const songArt = SongArt(player)
+
+    const seekbar = Widget.DrawingArea({
+        clickThrough: true,
+        drawFn: (_, cr, width, height) => {
+            const { color, variant } = MusicColor.value
+            // Lighten the color
+            const hsl = rgbToHSL(hexToRGB(color))
+            hsl[2] = Math.min(255, hsl[2] + (variant === 'dark' ? 30 : -20))
+            const rgb = hslToRGB(hsl).map((c) => c / 255)
+
+            // Mask with a rounded rectangle based on a border radius
+            const borderRadius = config.theme.borderRadius
+            cr.arc(borderRadius, borderRadius, borderRadius, Math.PI, (3 * Math.PI) / 2)
+            cr.arc(width - borderRadius, borderRadius, borderRadius, (3 * Math.PI) / 2, 0)
+            cr.arc(width - borderRadius, height - borderRadius, borderRadius, 0, Math.PI / 2)
+            cr.arc(borderRadius, height - borderRadius, borderRadius, Math.PI / 2, Math.PI)
+            cr.closePath()
+            cr.clip()
+
+            // Draw the line
+            cr.setSourceRGBA(...rgb, 1)
+            cr.rectangle(0, height - 2, width * (player.position / player.length), height)
+            cr.fill()
+        },
+    })
+        .poll(2000, (seekbar) => player.play_back_status === 'Playing' && seekbar.queue_draw())
+        .hook(player, (seekbar) => seekbar.queue_draw())
+
+    return Widget.Overlay({
+        passThrough: true,
+        child: TransparentButton({
+            css: MusicColor.bind('value').as(
+                ({ color, variant }) => `
+          background: ${color};
+          color: ${variant === 'dark' ? 'white' : 'black'};
+          font-size: 10px;
+          padding: 0px; 
+          padding-left: 12px;
+        `,
+            ),
+            onClicked: () => player.playPause(),
+            onMiddleClick: () => copySpotifyURL(player.metadata['xesam:url']),
+            onSecondaryClick: () => player.shuffle(),
+            onScrollUp: () => changeSpotifyVolume(true),
+            onScrollDown: () => changeSpotifyVolume(false),
+            child: Row([playPauseIcon, titleAndArtist, songArt], { spacing: 12 }),
+        }),
+        overlays: [seekbar],
+    })
+}
+
+export default function Music() {
+    return Section(
+        [
+            Widget.Box({
+                children: mpris
+                    .bind('players')
+                    .as((p) => p.filter((player) => player.name === 'spotify').map(Player)),
+            }),
+        ],
+        { margin: 0 },
+    )
+}
