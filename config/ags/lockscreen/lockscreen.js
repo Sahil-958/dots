e22c57c5391bef78d6935d49b18074739dbbc92d
Sahil <118348625+Sahil-958@users.noreply.github.com>
2024-07-02 13:56:37 +0530

    using astal auth for lockscreen

--- config/ags/lockscreen/lockscreen.js
+++ config/ags/lockscreen/lockscreen.js
@@ -1,12 +1,58 @@
 import Lock from "gi://GtkSessionLock";
 import Gdk from "gi://Gdk?version=3.0";
 import Gtk from "gi://Gtk?version=3.0";
+import AstalAuth from "gi://AstalAuth";
 
 import Media from "../media/media.js";
 import { RoundedAngleEnd, RoundedCorner } from "../roundedCorner/roundedCorner.js";
 
 App.applyCss(`${App.configDir}/lockscreen.css`);
 App.applyCss(`${App.configDir}/../media/media.css`);
+
+const auth = new AstalAuth.Pam();
+const info = Variable("");
+const inputNeeded = Variable(false);
+const inputVisible = Variable(false);
+
+auth.connect("auth-info", (auth, msg) => {
+    if (msg) {
+        loginStatusRevealer.reveal_child = true;
+        info.setValue(msg);
+    }
+    auth.supply_secret(null);
+});
+
+auth.connect("auth-error", (auth, msg) => {
+    info.setValue(msg);
+    console.error(msg);
+    auth.supply_secret(null);
+});
+
+auth.connect("auth-prompt-visible", (auth, msg) => {
+    inputNeeded.setValue(true);
+    inputVisible.setValue(true);
+    entry.grab_focus();
+    entry.text = "";
+});
+
+auth.connect("auth-prompt-hidden", (auth, msg) => {
+    inputNeeded.setValue(true);
+    inputVisible.setValue(false);
+    entry.grab_focus();
+    entry.text = "";
+});
+
+auth.connect("success", unlock);
+
+auth.connect("fail", p => {
+    entry.parent.toggleClassName("Failed", true);
+    loginStatusRevealer.reveal_child = true;
+    loginStatusRevealer.child.label = `Authentication Failed: ${p.username}`;
+    Utils.timeout(2000, () => {
+        loginStatusRevealer.reveal_child = false;
+    });
+    auth.start_authenticate();
+});
 
 const lock = Lock.prepare_lock();
 const windows = [];
@@ -28,18 +74,39 @@ Utils.timeout(15000, () => {
     unlock();
 });
 
+const entry = Widget.Entry({
+    class_name: "LoginEntry",
+    placeholderText: "Authentication Key",
+    primaryIconName: "system-lock-screen-symbolic",
+    xalign: 0.5,
+    hexpand: true,
+    visibility: inputVisible.bind(),
+    has_focus: false,
+    sensitive: inputNeeded.bind(),
+    on_change: self => {
+        if (!self.text) return;
+        loginStatusRevealer.reveal_child = false;
+    },
+    on_accept: self => {
+        self.parent.toggleClassName("Failed", false);
+        inputNeeded.setValue(false);
+        auth.supply_secret(self.text);
+        self.text = "******";
+    }
+}).on("realize", (entry) => entry.grab_focus());
+
+const loginStatusRevealer = Widget.Revealer({
+    class_name: "LoginStatusRevealer",
+    reveal_child: false,
+    transition: "slide_down",
+    transition_duration: 400,
+    child: Widget.Label({
+        class_name: "LoginStatusLabel",
+        label: info.bind(),
+    }),
+});
 
 const LoginBox = () => {
-    const loginStatusRevealer = Widget.Revealer({
-        class_name: "LoginStatusRevealer",
-        reveal_child: false,
-        transition: "slide_down",
-        transition_duration: 400,
-        child: Widget.Label({
-            class_name: "LoginStatusLabel",
-            label: "Invalid Authentication Key",
-        }),
-    });
 
     return Widget.Box({
         class_name: "LoginBox",
@@ -59,41 +126,7 @@ const LoginBox = () => {
                 hexpand: true,
                 vertical: true,
                 children: [
-                    Widget.Entry({
-                        class_name: "LoginEntry",
-                        placeholderText: "Authentication Key",
-                        primaryIconName: "system-lock-screen-symbolic",
-                        xalign: 0.5,
-                        hexpand: true,
-                        visibility: false,
-                        on_change: self => {
-                            if (!self.text) return;
-                            loginStatusRevealer.reveal_child = false;
-                        },
-                        on_accept: self => {
-                            self.parent.toggleClassName("Failed", false);
-                            self.sensitive = false;
-                            loginStatusRevealer.reveal_child = true;
-                            loginStatusRevealer.child.label = "Verifying Credentials...";
-                            Utils.timeout(500, () => {
-                                loginStatusRevealer.reveal_child = false;
-                            });
-                            Utils.authenticate(self.text ?? "")
-                                .then(() => {
-                                    Utils.timeout(500, () => {
-                                        return unlock();
-                                    });
-                                }).catch(e => {
-                                    self.parent.toggleClassName("Failed", true);
-                                    loginStatusRevealer.reveal_child = true;
-                                    loginStatusRevealer.child.label = e.message;
-                                    self.text = "";
-                                    self.placeholder_text = e.message;
-                                    self.sensitive = true;
-                                    self.grab_focus();
-                                });
-                        }
-                    }).on("realize", (entry) => entry.grab_focus()),
+                    entry,
                     loginStatusRevealer,
                 ]
             }),
@@ -145,7 +178,9 @@ const overlayChild = Widget.Revealer({
             }),
         ],
     }),
-}).on("realize", self => Utils.idle(() => self.reveal_child = true));
+}).on("realize", self => Utils.idle(() => {
+    Utils.timeout(600, () => self.reveal_child = true);
+}));
 
 
 const revealerChild = Widget.Box({
@@ -221,6 +256,8 @@ function on_finished() {
     Gdk.Display.get_default()?.sync();
     App.quit();
 }
+
 // lock.connect("locked", on_locked);
 lock.connect("finished", on_finished);
 lock_screen();
+auth.start_authenticate();
