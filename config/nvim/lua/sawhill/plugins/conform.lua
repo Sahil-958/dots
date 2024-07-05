b1642db0a4bd6af25b0f96aac0496b32900b06f3
Sahil <118348625+Sahil-958@users.noreply.github.com>
2024-07-05 11:35:58 +0530

    enabled formatters for js and ts

--- config/nvim/lua/sawhill/plugins/conform.lua
+++ config/nvim/lua/sawhill/plugins/conform.lua
@@ -6,8 +6,8 @@ return {
 
         conform.setup({
             formatters_by_ft = {
-                -- javascript = { "prettier" },
-                -- typescript = { "prettier" },
+                javascript = { "prettier" },
+                typescript = { "prettier" },
                 -- javascriptreact = { "prettier" },
                 -- typescriptreact = { "prettier" },
                 -- svelte = { "prettier" },
