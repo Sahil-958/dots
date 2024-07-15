42aca944ec4522ddd574757197ad2ce14183ff28
Sahil <118348625+Sahil-958@users.noreply.github.com>
2024-07-15 20:49:54 +0530

    added arg to pass theme mode

--- scripts/matugenWall.sh
+++ scripts/matugenWall.sh
@@ -2,14 +2,18 @@
 #check if $2 arg is empty
 image=$1
 scheme=$2
+theme=$3
 if [ "$2" = "" ]; then
     scheme="scheme-content"
 fi
 if [ "$1" = "" ]; then
     exit 1
 fi
+if [ "$3" = "" ]; then
+    theme="dark"
+fi
 
-matugen image "$image" -t "$scheme"
+matugen image "$image" -t "$scheme" -mode "$theme"
 # using pywal for konsole and other 
 wal -i "$image"
 ~/dots/scripts/matugenUpdateRest.sh &
