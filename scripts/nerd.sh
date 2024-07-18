0344db4fe9d5ec240b8f4180d194b441f1ebfb4d
Sahil <118348625+Sahil-958@users.noreply.github.com>
2024-07-18 16:44:37 +0530

    term version of nerd.sh

---
+++ scripts/nerd.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+case $1 in
+    update)
+        curl -fsSL https://raw.githubusercontent.com/ryanoasis/nerd-fonts/master/glyphnames.json > ~/dots/scripts/glyphnames.json && echo "glyphnames.json updated"
+        ;;
+    *)
+        jq -r '. | to_entries[] | select(.key != "METADATA") | "\(.value.char)   \(.key) \(.value.code)"' ~/dots/scripts/glyphnames.json |fzf|wl-copy
+        ;;
+esac
