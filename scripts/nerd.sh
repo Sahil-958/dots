12985da8718da05b5b2716abc721ad9d02a912b7
Sahil <118348625+Sahil-958@users.noreply.github.com>
2024-07-21 13:07:11 +0530

    fix: forget to extract the selected glpyh

--- scripts/nerd.sh
+++ scripts/nerd.sh
@@ -5,6 +5,6 @@ case $1 in
         curl -fsSL https://raw.githubusercontent.com/ryanoasis/nerd-fonts/master/glyphnames.json > ~/dots/scripts/glyphnames.json && echo "glyphnames.json updated"
         ;;
     *)
-        jq -r '. | to_entries[] | select(.key != "METADATA") | "\(.value.char)   \(.key) \(.value.code)"' ~/dots/scripts/glyphnames.json |fzf|wl-copy
+        jq -r '. | to_entries[] | select(.key != "METADATA") | "\(.value.char)   \(.key) \(.value.code)"' ~/dots/scripts/glyphnames.json |fzf| sed 's/^\(.\).*/\1/' |wl-copy
         ;;
 esac
